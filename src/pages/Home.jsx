2d95e404de1c4855496419be15807964fd2c5a09
Nishit Sangani <nishit.s@inheritx.com>
2025-12-11 16:05:15 +0530

    Space management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -337,7 +337,7 @@ function Home() {
               transition={{ duration: 0.8 }}
             >
               <motion.div
-                className="mb-6"
+                className="mb-3 md:mb-6"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.2, duration: 0.6 }}
